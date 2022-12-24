8aed6fb183c09efa0ad83b3bb2d5442a6ec805fe
Gregory Ogbemudia <gregory.ogbemudia@gmail.com>
2022-12-24 06:55:48 +0100

    updated authorization to retreive own posts only

--- 09-Fast-API-using-PostgreSQL-with-SQLAlchemy-ORM/app/routes/post.py
+++ 09-Fast-API-using-PostgreSQL-with-SQLAlchemy-ORM/app/routes/post.py
@@ -18,13 +18,14 @@ Notes on Using Dependencies:
 @router.post("/", response_model=schemas.Post)
 def create_post(post: schemas.PostCreate, db: Session = Depends(database.get_db), current_user: int = Depends(oauth2.get_current_user)):
 
+    # printing to see what is actually happening (abstraction) in the terminal
     print(current_user.id)  # just to print in the terminal the user id accessing the path.
     print(current_user.email)
 
     # # Instead of typing in all the fields like this...
     # new_post = models.Post(title=post.title, content=post.content, published=post.published)
 
-    # # use **post.dict() to unpack the post as a dict into the Post model like this...
+    # # use **post.dict() to unpack the post as a dict into the Post model like this, also sets the current user as the author
     new_post = models.Post(author_id=current_user.id, **post.dict())
     db.add(new_post)
     db.commit()
@@ -36,23 +37,44 @@ def create_post(post: schemas.PostCreate, db: Session = Depends(database.get_db)
 @router.get("/", response_model=List[schemas.Post])
 def get_posts(db: Session = Depends(database.get_db), current_user: int = Depends(oauth2.get_current_user)):
 
-    print(current_user)  # just to print in the terminal the user id accessing the path.
+    # printing to see what is actually happening (abstraction) in the terminal
+    print(current_user.id)  # to see the user id accessing the path in the terminal.
+    print(db.query(models.Post))
+    print(db.query(models.Post).filter(models.Post.author_id == current_user.id))
 
-    posts = db.query(models.Post).all()
-    print(db.query(models.Post))  # to see what actually happens (abstraction) in the terminal.
+    # # to Retrieve all users posts.
+    # posts = db.query(models.Post).all()
+
+    # to retrieve only logged-in user's posts.
+    posts = db.query(models.Post).filter(models.Post.author_id == current_user.id).all()
+
     return posts
 
 
 # Retrieve a Post by id (with SQLAlchemy ORM)
 @router.get("/{id}", response_model=schemas.Post)
 def get_post(id: int, db: Session = Depends(database.get_db), current_user: int = Depends(oauth2.get_current_user)):
-    print(current_user)  # just to print in the terminal the user id accessing the path.
+
+    # printing to see what is actually happening (abstraction) in the terminal
+    print(current_user.id)  # to see the user id accessing the path in the terminal.
+    print(db.query(models.Post).filter(models.Post.id == id))
+
+    # to retrieve any post by any user
     post = db.query(models.Post).filter(models.Post.id == id).first()
+
     # you can also use this...
     # post = db.query(models.Post).get(id)
 
+    # # to retrieve ONLY logged-in user's post.
+    # post = db.query(models.Post).filter(models.Post.id == id).first()
+
+    # check if post exists
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id: {id} was not found")
+
+    # # check if current user is the author retrieving own post
+    # if post.author_id != current_user.id:
+    #     raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not authorized to perform requested action")
 
     return post
 
@@ -60,6 +82,7 @@ def get_post(id: int, db: Session = Depends(database.get_db), current_user: int 
 # Delete a Post (with SQLAlchemy ORM)
 @router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(id: int, db: Session = Depends(database.get_db), current_user: int = Depends(oauth2.get_current_user)):
+
     print(current_user.id)  # just to print in the terminal the user id accessing the path.
 
     post_query = db.query(models.Post).filter(models.Post.id == id)
@@ -71,10 +94,10 @@ def delete_post(id: int, db: Session = Depends(database.get_db), current_user: i
 
     # check if current user is the author
     if post.author_id != current_user.id:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not authorized to delete post")
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not authorized to perform requested action")
 
     # db.delete(post)  # this also works, instead of this...
-    post_query.delete(synchronize_session=False)  # calling the query.delete method works without the .first() attribute
+    post_query.delete(synchronize_session=False)  # NB: query.delete method is only performed on the query and not the post itself.
     db.commit()
 
     return Response(status_code=status.HTTP_204_NO_CONTENT)
@@ -88,18 +111,18 @@ def delete_post(id: int, db: Session = Depends(database.get_db), current_user: i
 def update_post(id: int, updated_post: schemas.PostUpdate, db: Session = Depends(database.get_db), current_user: int = Depends(oauth2.get_current_user)):
     print(current_user)  # just to print in the terminal the user id accessing the path.
 
-    post_qyery = db.query(models.Post).filter(models.Post.id == id)
+    post_query = db.query(models.Post).filter(models.Post.id == id)
 
-    post = post_qyery.first()
+    post = post_query.first()
 
     if post is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id: {id} not found")
 
     # check if current user is the author
     if post.author_id != current_user.id:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not authorized to delete post")
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not authorized to perform requested action")
 
-    post_qyery.update(updated_post.dict(), synchronize_session=False)  # calling the query.update method works without the .first() attribute
+    post_query.update(updated_post.dict(), synchronize_session=False)  # NB: query.update method is only performed on the query and not the post itself.
     db.commit()
 
     return post
